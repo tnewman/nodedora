e3d82501e1c2ba6980fc9bd9565368f4af214085
Thomas Newman <tnewman@users.noreply.github.com>
2019-03-03 14:30:04 -0500

    Added Pandora Client skeleton

---
+++ src/PandoraClient.js
@@ -0,0 +1,5 @@
+class PandoraClient {
+
+}
+
+module.exports = PandoraClient;
---
+++ test/PandoraClient.spec.js
@@ -0,0 +1,21 @@
+const PandoraClient = require('../src/PandoraClient');
+
+describe('Pandora Client', () => {
+  const pandoraClient = new PandoraClient();
+
+  it('should authenticate with valid credentials', () => {
+
+  });
+
+  it('should throw an exception with invalid credentials', () => {
+
+  });
+
+  it('should display the list of current stations', () => {
+
+  });
+
+  it('should display the list of songs for the current station', () => {
+
+  });
+});
