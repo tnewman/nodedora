b02d7d4f3b35deca7b4a34f6561bece76f170c88
Thomas Newman <tnewman@users.noreply.github.com>
2019-05-17 19:05:31 -0400

    Remove legacy project

--- src/configuration/Configuration.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Configuration {
-  constructor(stationId) {
-    this.stationId = stationId;
-  }
-
-  static fromJSON(json) {
-    return new Configuration(json.stationId);
-  }
-};
