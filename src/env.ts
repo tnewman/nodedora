dbd54dfc47e7d546c1bde17889587322ba4a8d40
Thomas Newman <tnewman@users.noreply.github.com>
2020-05-24 14:50:14 -0400

    Remove existing project

--- src/env.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-
-import logger from './logger';
-
-logger.info('Loading environment variables from .env');
-
-config();
--- src/pandora-client/station.ts
+++
@@ -1,9 +0,0 @@
-export default class Station {
-    readonly stationId: string;
-    readonly name: string;
-
-    constructor(station: Station) {
-        this.stationId = station.stationId;
-        this.name = station.name;
-    }
-}
